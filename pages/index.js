9ab58e044abae7d947e1ec00d21c01c9b7f25f1d
Mali Matiso <s220651337@mandela.ac.za>
2022-12-01 19:08:28 +0200

    Added textarea for prompts

--- pages/index.js
+++ pages/index.js
@@ -17,6 +17,9 @@ const Home = () => {
                         <h2>insert your subtitle here</h2>
                     </div>
                 </div>
+                <div className="prompt-container">
+                    <textarea placeholder="What's on your mind?" className="prompt-box" />
+                </div>
             </div>
             <div className="badge-container grow">
                 <a
